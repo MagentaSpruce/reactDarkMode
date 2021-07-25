0aa79e347a6762872c72dd945a1bbb0d4731b392
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-25 13:48:49 -0400

    Updates

---
+++ precache-manifest.c8689c427094e60c264bb7857f6dcea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e2d10016347ec565490a7f5cdad176",
+    "url": "/reactDarkMode/index.html"
+  },
+  {
+    "revision": "bfb18913af3cd3ed8dd0",
+    "url": "/reactDarkMode/static/css/main.bc3147d9.chunk.css"
+  },
+  {
+    "revision": "4646610705680ce7d806",
+    "url": "/reactDarkMode/static/js/2.a99e8b18.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/reactDarkMode/static/js/2.a99e8b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb18913af3cd3ed8dd0",
+    "url": "/reactDarkMode/static/js/main.ab174020.chunk.js"
+  },
+  {
+    "revision": "c536bfa092b958c342ed",
+    "url": "/reactDarkMode/static/js/runtime-main.ee14653a.js"
+  }
+]);
